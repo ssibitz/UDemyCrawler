0597d4d7d898a3f18210db845c79df0e9a578bd8
Stefan Sibitz <ssibitz@me.com>
2022-07-11 13:34:39 +0200

    # Updated BuildScript for building the exe-file

--- BuildExe.cmd
+++ BuildExe.cmd
@@ -4,4 +4,4 @@ echo.
 REM pip install auto-py-to-exe
 REM auto-py-to-exe
 set CURR_PATH=%cd%
-pyinstaller --noconfirm --onefile --windowed --icon "%CURR_PATH%/UDemyCrawler.ico" --add-data "%CURR_PATH%/UDemyCrawler.ico;."  "%CURR_PATH%/UDemyCrawler.py"
+pyinstaller --noconfirm --onefile --windowed --icon "%CURR_PATH%/res/UDemyCrawler.ico" --add-data "%CURR_PATH%/res;res/" --add-data "%CURR_PATH%/icons;icons/"  "%CURR_PATH%/UDemyCrawler.py"
